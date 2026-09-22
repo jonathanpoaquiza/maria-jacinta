1a4eaeab50b8cd46005582d9e2732bdc24f86d81
jonathanpoaquizaepn <poaquizajonathan75@gmail.com>
2026-09-22 11:16:40 -0500

    Update: Fix page problems

--- frontend/react-ts/src/main.tsx
+++ frontend/react-ts/src/main.tsx
@@ -13,7 +13,7 @@ import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/maria-jacinta/frontend/react-ts">
+    <BrowserRouter basename="/maria-jacinta/">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
